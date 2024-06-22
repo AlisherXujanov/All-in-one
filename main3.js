b3a12070a4fe99335552c581f19d975261f62a21
Alisher <alisherxujanov163@gmail.com>
2024-06-22 20:26:43 +0500

    Davron lesson

--- main3.js
+++ main3.js
@@ -1,12 +1,4 @@
-let numbers = [1, 22, 122, 1, 11, 43, 23, 123]
-let purpose = 44
-
-let result = numbers.filter((num,idx) => {
-    let needed = purpose - num
-    return numbers.includes(needed) && num !== needed
-})
-if (result.length === 2) {
-    console.log(result + " = " + purpose)
-} else {
-    console.log("No two numbers to target.")
+function delKey(obj, key) {
+    delete obj[key];
+    return obj;
 }
