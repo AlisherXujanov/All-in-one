0d385be56cf027324eee328b7c2c72e938cebdaf
Alisher <alisherxujanov163@gmail.com>
2024-07-16 18:20:30 +0500

    Fariza lessons

--- Fariza-lessons/4-algorithms/exercises-2.js
+++ Fariza-lessons/4-algorithms/exercises-2.js
@@ -3,3 +3,190 @@ let URL = "https://www.w3resource.com/javascript-exercises/javascript-recursion-
 
 
 
+// ===========================================================
+// ===========================================================
+//ex2
+function divisionNums(a, b) {
+    if (b === 0) {
+        return a
+    }
+    else {
+        return divisionNums(b, a % b)
+        // 30,  50
+        // 50,  30
+        // 30,  20
+        // 20,  10
+        // 10,  0
+    }
+}
+let fff = divisionNums(30, 50)
+// console.log(fff)
+// ===========================================================
+// ===========================================================
+//ex4
+var arrays = [1, 2, 3, 4, 5, 6]
+// function sum(arr) {
+//     let sum = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i]
+//     }
+//     console.log(sum)
+// }
+function sumRecursively(arr) {
+    let first = arr[0]
+    if (arr.length == 1) {
+        return first
+    }
+    return first + sumRecursively(arr.slice(1))
+}
+// 1 + sumRecursively([2, 3, 4, 5, 6])
+// 1 + 2 + sumRecursively([3, 4, 5, 6])
+// 1 + 2 + 3 + sumRecursively([4, 5, 6])
+// 1 + 2 + 3 + 4 + sumRecursively([5, 6])
+// 1 + 2 + 3 + 4 + 5 + sumRecursively([6])
+// 1 + 2 + 3 + 4 + 5 + 6
+let r = sumRecursively(arrays)
+// console.log("Exercise 4: ", r)
+// ===========================================================
+// ===========================================================
+// ex5
+function getRange(x, y) {
+    let next = x + 1
+    if (next == y) {
+        return []
+    }
+    return [next, ...getRange(next, y)]
+}
+// --------
+// 2, 9   |
+// --------
+// [3, ...getRange(3, 9)]
+// [3, 4, ...getRange(4, 9)]
+// [3, 4, 5, ...getRange(5, 9)]
+// [3, 4, 5, 6, ...getRange(6, 9)]
+// [3, 4, 5, 6, 7, ...getRange(7, 9)]
+// [3, 4, 5, 6, 7, 8, ...getRange(8, 9)]
+// [3, 4, 5, 6, 7, 8]
+r = getRange(2, 9)
+// console.log("Exercise 5: ", r)
+// ===========================================================
+// ===========================================================
+//ex7
+let numbers1 = [1, 2, 3, 4, 5, 6, 7, 8]
+function findEvens(nums) {
+    let even = []
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 === 0) {
+            even.push(nums[i])
+        }
+    }
+    return even
+}
+let resOfEvens = findEvens(numbers1)
+console.log(resOfEvens)
+// ===========================================================
+// ===========================================================
+//ex9
+let unsortedNums = [34, 7, 23, 32, 5, 62]
+function sortArray(nums) {
+    return nums.sort((a, b) => a - b)
+}
+let resOfSort = sortArray(unsortedNums)
+console.log("Simple sort: ", resOfSort)
+
+let bubbleSort = [5, 6, 4, 2, 1, 8]
+function bubbleSortNums(arr) {
+    let sorted = false
+    while (!sorted) {
+        sorted = true
+        for (let i = 0; i < arr.length + 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+                sorted = false
+            }
+        }
+    }
+    return arr
+}
+let d = bubbleSortNums(bubbleSort)
+console.log("Bubble sort: ", d)
+// ===========================================================
+// ===========================================================
+//ex10
+let str = "A man, a plan, a canal - Panama"
+function isPolindrom(str) {
+    // TODO:  decrease the amount of code for optimization
+    let sp = str.replace(/[^A-Z0-9]/gi, "").toLowerCase().split("").reverse().join("")
+    console.log('sp', sp)
+    for (let i = 0; i < str.length; i++) {
+        let st = str.replace(/[^A-Z0-9]/gi, "").toLowerCase().split("").join("")
+        console.log('st', str)
+        if (st[i] === sp[i]) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+let resOfPolindrome = isPolindrom(str)
+console.log(resOfPolindrome)
+// ===========================================================
+// ===========================================================
+
+
+//ex
+let binary = [0, 1, 2, 3, 4, 5, 6]
+let targetNum = 5
+let found = false
+function binarySearch(arr, target) {
+    let start = 0
+
+    let end = arr.length
+    while (end - start > 0) {
+        let middleItem = Math.floor((start + end) / 2)
+        if (arr[middleItem] === target) {
+            found = true
+            break
+        } else {
+            if (target > arr[middleItem]) {
+                start = middleItem + 1
+            } else {
+                end = middleItem - 1
+            }
+        }
+    } if (found) {
+        console.log('target number exist', target)
+    } else {
+        console.log('Target number exist')
+    }
+
+}
+binarySearch(binary, targetNum)
+
+
+//ex
+let binaryNum = [0, 1, 2, 3, 4, 5, 6]
+let target1 = 4
+let find = false
+function findTargetNum(arr, target) {
+    let start = 0
+    let end = arr.length
+    while (end - start > 0) {
+        let middle = parseInt((end + start) / 2)
+        if (target === arr[middle]) {
+            find = true
+            break
+        } else {
+            if (target > arr[middle]) {
+                start = middle + 1
+            } else {
+                end = middle - 1
+            }
+        }
+    }
+    if (find) {
+        console.log('target number exist')
+    } else {
+        console.log('Target number exist', target)
+    }
+}
--- Fariza-lessons/5-diff-arr-methods/main.js
+++ Fariza-lessons/5-diff-arr-methods/main.js
@@ -1,6 +1,77 @@
+// ADVANCED METHODS OF ARRAYS
+// find || filter || map || reduce
+// ===========================================================
+// ===========================================================
+// find & filter & map - SYNTAX  =>   (item, index, array) => {} 
+// ===========================================================
+// reduce  -  SYNTAX   =>   [...].reduce((acc, next) => { return == acc })
+//                          [...].reduce((acc, next) => { return == acc }, 0)
+// ===========================================================
+// ===========================================================
+// 1. FIND  ->  finds an item in an array by condition
+//          ->  finds the first item and stops
+//    UZ: massivda shartni tekshiradi va shartni qanoatlantiradigan biror elementni topadi
+//        birinchi elementni topadi va to'xtaydi
+let fruits = ['apple', 'banana', 'grapes', 'mango', 'orange', 'peach']
 
-// map
-// filter
-// find
-// findIndex
-// reduce
+let result = fruits.find((item, index, array) => {
+    let lastLetter = item[item.length-1].toLowerCase()
+    if (!"aioue".includes(lastLetter)) {
+        return item
+    }
+})
+// console.log("Found item: ", result)
+// ===========================================================
+// ===========================================================
+// 2. FILTER   ->  finds all items in an array by condition
+//             ->  finds all items and returns them
+//    UZ: massivda shartni tekshiradi va shartni qanoatlantiradigan barcha elementlarni topadi
+//        barcha elementlarni topadi va qaytaradi
+result = fruits.filter((item, index, array) => {
+    let lastLetter = item[item.length-1].toLowerCase()
+    if (!"aioue".includes(lastLetter)) {
+        return item
+    }
+})
+// console.log("Found items: ", result)
+// ===========================================================
+// ===========================================================
+// 3. MAP  ->   creates a new array by all items but can change them on the way
+//           UZ: Massivni hamma elementlarini o'zgartirib yangi massiv yaratadi
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let square_numbers = numbers.map((item, index, array) => {
+    return item ** 2
+})
+// console.log("Square numbers: ", square_numbers)
+// ===========================================================
+// ===========================================================
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let sum = numbers.reduce((acc, next) => { 
+    // acc == 1,    next == 2     =>  3
+    // ---------------------------------
+    // acc == 3,    next == 3     =>  6
+    // ---------------------------------
+    // acc == 6,    next == 4     =>  10
+    // ---------------------------------
+    // acc == 10,   next == 5     =>  15
+    // ---------------------------------
+    // acc == 15,   next == 6     =>  21
+    // ...
+    // acc == 36,   next == 9     =>  45
+    return acc + next
+})
+// console.log("Sum: ", sum)
+// -------------------------------------
+numbers = [10, 5, 2, 19, 55, 52, 6, 7, 8, 9]
+let maxNum = numbers.reduce((acc,next) => {
+    // acc == 10,   next == 5    =>   10
+    // acc == 10,   next == 2    =>   10
+    // ... 
+    if(acc < next){
+        return next
+    } else{
+        return acc
+    }
+})
+// console.log("maxNum: ", maxNum)
+// -------------------------------------
