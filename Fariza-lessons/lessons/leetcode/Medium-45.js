e039e68094f56452c8f29319d5dcf623f384bcd6
Alisher <alisherxujanov163@gmail.com>
2024-08-03 08:57:11 +0500

    leetcode 274

--- Fariza-lessons/lessons/leetcode/Medium-45.js
+++ Fariza-lessons/lessons/leetcode/Medium-45.js
@@ -32,24 +32,36 @@
 // ====================================================================================
 // ====================================================================================
 /**
- * @param {number[]} nums
- * @return {number}
+ * Calculates the minimum number of jumps needed to reach the last index of the given array.
+ *
+ * @param {number[]} nums - The array of numbers representing the maximum jump length at each position.
+ * @returns {number} - The minimum number of jumps needed.
  */
 var jump = function (nums) {
-    // The starting range of the first jump is [0, 0]
-    let answer = 0, n = nums.length
-    let curEnd = 0, curFar = 0
+    let answer = 0; // minimum number of jumps needed
+    let curEnd = 0; // current maximum reachable index
+    let curFar = 0; // maximum reachable index from the current position
 
-    for (let i = 0; i < n - 1; ++i) {
-        // Update the farthest reachable index of this jump.
-        curFar = Math.max(curFar, i + nums[i])
+    for (let i = 0; i < nums.length - 1; ++i) { // Iterate through the array
+        curFar = Math.max(curFar, i + nums[i]); // Update the maximum reachable index from the current position
 
-        // If we finish the starting range of this jump,
-        // Move on to the starting range of the next jump.
-        if (i === curEnd) {
-            ++answer
-            curEnd = curFar
+        if (i === curEnd) { // If the current position is equal to the current maximum reachable index
+            ++answer; // Increment the minimum number of jumps needed
+            curEnd = curFar; // Update the current maximum reachable index
         }
     }
-    return answer
+    return answer; // Return the minimum number of jumps needed
+
+    // steps = [2, 3, 1, 1, 4]
+    // curFar == 0 && 0+2 > 0 => curFar = 2      //          0===0  => answer = 1, curEnd = 2
+    // curFar == 2 && 1+3 > 2 => curFar = 4      //          1===2  => answer = 1, curEnd = 2
+    // curFar == 4 && 2+1 > 4 => curFar = 4      //          2===2  => answer = 2, curEnd = 4
+    // curFar == 4 && 3+1 > 4 => curFar = 4      //          3===4  => answer = 2, curEnd = 4
+    // curFar == 4 && 4+4 > 4 => curFar = 8      //          4===8  => answer = 2, curEnd = 4
+    // return 2
 }
+
+
+steps = [2, 3, 1, 1, 4]
+let r = jump(steps)
+console.log(r)  // 2
