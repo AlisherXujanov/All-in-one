c05efa4ba75ec02441eac3d60b824460c19ccdd8
Alisher <alisherxujanov163@gmail.com>
2024-08-10 22:10:34 +0500

    update

--- Madina-lessons/7-loops/loops.js
+++ Madina-lessons/7-loops/loops.js
@@ -1,13 +1,35 @@
-// 1. creates a control vars
-// 2. condition
-// 3. controls first expression
+// 1st type of loop 
+// STANDARD FOR-LOOP
+//     1. creates a control vars    (control qilish uchun o'zgaruvchi yaratadi)
+//     2. condition                 (shart yaratiladi)
+//     3. controls first expression (birinchi o'zgaruvchi kontrol qiladi)
 
-// for (let index = 0; index <= 6; index++) {
+
+// for (let i=0;  i<5;  i++) {
+//     console.log("Hello")
+//     console.log("Madina")
+// }
+// index == 0;   true   =>   JS will read our code   =>  index+=1
+// index == 1;   true   =>   JS will read our code   =>  index++
+// index == 2;   true   =>   JS will read our code   =>  index++
+// index == 3;   true   =>   JS will read our code   =>  index++
+// index == 4;   true   =>   JS will read our code   =>  index++
+// index == 5;   false  =>   JS will stop reading our code
+
+
+// for (let index = 0; index < 5; index++) {
 //     console.log("Index: ", index)
 // }
+// 0
+// 1
+// 2
+// 3
+// 4
+
 // -----------------------------------------------------------
 // let fruits = ['apple', 'banana', 'orange', 'kiwi', 'mango']
 
+// fruits.length == 5
 // for (let i=0; i<fruits.length; i++) {
 //     console.log(fruits[i].toUpperCase())
 // }
@@ -17,13 +39,46 @@
 // console.log(fruits[3].toUpperCase())
 // console.log(fruits[4].toUpperCase())
 // ...
-// -----------------------------------------------------------
-// for ... of ...
 
-// let fruits = ['apple', 'banana', 'orange', 'kiwi', 'mango']
-
-// for (let item of fruits) {
-//     console.log(item.toUpperCase())
+// Create a loop that prints out the numbers from 0 to 10 (both included)
+// ...
+// for (let i = 0; i < 11; i++) {
+//     console.log(i)
 // }
 
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ********
+// *********
+// for (let i = 1; i < 11; i++){
+//     console.log("*".repeat(i))
+// }
+// ------------------------------
+// **********
+// *********
+// ********
+// *******
+// ******
+// *****
+// ****
+// ***
+// **
+// *
+// for (let i = 10; i > 0; i--) {
+//     console.log("*".repeat(i))
+// }
+// -----------------------------------------------------------
+// for ... of ...
+let fruits = ['apple', 'banana', 'orange', 'kiwi', 'mango']
 
+for (let el of fruits) {
+    console.log(el.toUpperCase())
+}
+
+// -----------------------------------------------------------
+// for ... in ...
