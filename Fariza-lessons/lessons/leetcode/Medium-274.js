4c223fb0fa6ab9fb51a839274a5cfe4a86260e21
Alisher <alisherxujanov163@gmail.com>
2024-08-19 13:18:22 +0500

    update

--- Fariza-lessons/lessons/leetcode/Medium-274.js
+++ Fariza-lessons/lessons/leetcode/Medium-274.js
@@ -1,6 +1,7 @@
 // Given an array of integers citations where citations[i] is the number of citations a 
 // researcher received for their ith paper, return the researcher's h-index.
 
+
 // According to the definition of h-index on Wikipedia: The h-index is defined as 
 // the maximum value of h such that the given researcher has published at least h 
 // papers that have each been cited at least h times.
@@ -23,6 +24,12 @@
 
 // ========== ========== ========== ========== ========== ========== ========== ========== 
 // ========== ========== ========== ========== ========== ========== ========== ========== 
+// Example 3:
+// Input: citations = [1, 11, 12, 5, 7, 8, 9, 10]
+// Output: 6
+
+// ========== ========== ========== ========== ========== ========== ========== ========== 
+// ========== ========== ========== ========== ========== ========== ========== ========== 
 // Constraints:
 // n == citations.length
 // 1 <= n <= 5000
@@ -39,16 +46,17 @@
 var hIndex = function (citations) {
     // Sort the citations array in descending order.
     citations.sort((a, b) => b - a);
-    
+
     // Initialize the h-index to 0.
     let hIndex = 0;
-    
+
     // Iterate through the sorted citations array.
     for (let i = 0; i < citations.length; i++) {
         // Check if the current citation count is greater than or equal to the current index + 1.
-        if (citations[i] >= i + 1) {
-            // Update the h-index to the current index + 1.
-            hIndex = i + 1;
+        const CURRENT_INDEX = i + 1;
+        if (citations[i] >= CURRENT_INDEX) {
+            // Update the h-index to the CURRENT index + 1.
+            hIndex = CURRENT_INDEX;
         }
     }
     // [3,0,6,1,5]   => [6, 5, 3, 1, 0]
@@ -58,6 +66,10 @@ var hIndex = function (citations) {
     // i = 3, 1 >= 4 => hIndex = 3
     // i = 4, 0 >= 5 => hIndex = 3
     // Return the calculated h-index.
-    
+
     return hIndex;
 };
+
+
+s = [3, 0, 6, 1, 5]
+console.log(hIndex(s)) // 3
--- Fariza-lessons/lessons/leetcode/Medium-45.js
+++ Fariza-lessons/lessons/leetcode/Medium-45.js
@@ -40,24 +40,24 @@
 var jump = function (nums) {
     let answer = 0; // minimum number of jumps needed
     let curEnd = 0; // current maximum reachable index
-    let curFar = 0; // maximum reachable index from the current position
+    let currentPlace = 0; // maximum reachable index from the current position
 
     for (let i = 0; i < nums.length - 1; ++i) { // Iterate through the array
-        curFar = Math.max(curFar, i + nums[i]); // Update the maximum reachable index from the current position
+        currentPlace = Math.max(currentPlace, i + nums[i]); // Update the maximum reachable index from the current position
 
         if (i === curEnd) { // If the current position is equal to the current maximum reachable index
             ++answer; // Increment the minimum number of jumps needed
-            curEnd = curFar; // Update the current maximum reachable index
+            curEnd = currentPlace; // Update the current maximum reachable index
         }
     }
     return answer; // Return the minimum number of jumps needed
 
     // steps = [2, 3, 1, 1, 4]
-    // curFar == 0 && 0+2 > 0 => curFar = 2      //          0===0  => answer = 1, curEnd = 2
-    // curFar == 2 && 1+3 > 2 => curFar = 4      //          1===2  => answer = 1, curEnd = 2
-    // curFar == 4 && 2+1 > 4 => curFar = 4      //          2===2  => answer = 2, curEnd = 4
-    // curFar == 4 && 3+1 > 4 => curFar = 4      //          3===4  => answer = 2, curEnd = 4
-    // curFar == 4 && 4+4 > 4 => curFar = 8      //          4===8  => answer = 2, curEnd = 4
+    // currentPlace == 0 && 0+2 > 0 => currentPlace = 2      //          0===0  => answer = 1, curEnd = 2
+    // currentPlace == 2 && 1+3 > 2 => currentPlace = 4      //          1===2  => answer = 1, curEnd = 2
+    // currentPlace == 4 && 2+1 > 4 => currentPlace = 4      //          2===2  => answer = 2, curEnd = 4
+    // currentPlace == 4 && 3+1 > 4 => currentPlace = 4      //          3===4  => answer = 2, curEnd = 4
+    // currentPlace == 4 && 4+4 > 4 => currentPlace = 8      //          4===8  => answer = 2, curEnd = 4
     // return 2
 }
 
