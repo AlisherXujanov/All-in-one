e70c94fe3649550aeba7afa325b7a46f07f5e5d9
Alisher <alisherxujanov163@gmail.com>
2024-08-25 08:00:47 +0500

    update

--- Madina-lessons/8-Functions/Madina_hw.js
+++ Madina-lessons/8-Functions/Madina_hw.js
@@ -1,68 +1,99 @@
-// ----------------------------------------------------------------
-// ------------------------- INTERMEDIATE -------------------------
-// 5. Write a function that takes two arguments, a string and a letter,
-// and the function will count the number of this specified letter in the string.
+// TASK 1
+// function minutes_to_seconds(minutes) {
+//     return minutes * 60
+// }
+// result = minutes_to_seconds(15)
+// console.log(result)
 
-function countLetterInStr(str, target_letter) {
-    let pool = 0
-    // Steps:
-    // 1. Create a loop for taking each letter in the string
-    for (let i = 0; i < str.length; i++) { // 0-5,   0,1,2,3,4
-        // retrieve a letter by index that's i here
-        // Hello  =>   str[0]  =>  H
-        // Hello  =>   str[1]  =>  e
-        // ...
-        // Hello  =>   str[4]  =>  o
-        let letter = str.charAt(i)
-        // 2. Check if the letter is equal to the target_letter
-        if (letter == target_letter) {
-            // 3. If so, them increment the pool by 1
-            pool += 1
-        }
-    }
-    return pool
-}
-let text = "hello"
-let letter = "s"
-// let result = countLetterInStr(text, letter)
-// console.log(result) // 0
 
-text = "Hello world"
-letter = 'o'
-// result = countLetterInStr(text, letter)
-// console.log(result) // 2
 
-// ----------------------------------------------------------------
-// ----------------------------------------------------------------
-// ------------------------- ADVANCED -----------------------------
-// 1. Print stars (*) in a pyramid form with N number of steps.
-function create_pyramid_n_lines(lines) {
-    for (let i = 0; i <= lines; i++) {
-        // ----*
-        // ---* *
-        // --* * *
-        // -* * * *
-        // * * * * *
-        // let needed_spaces = lines - i
-        // console.log(" ".repeat(needed_spaces) + "*".repeat(i))
-        // ========================================================================
-        //       *
-        //      ***
-        //     *****
-        //    *******
-        //   *********
-        let needed_spaces = lines - i
-        console.log(" ".repeat(needed_spaces) + "*".repeat(2 * i + 1))
-        // i  ==  0    =>   2 * 0 + 1  =>  1            *
-        // i  ==  1    =>   2 * 1 + 1  =>  3           ***
-        // i  ==  2    =>   2 * 2 + 1  =>  5          *****
-        // i  ==  3    =>   2 * 3 + 1  =>  7         *******
-        // i  ==  4    =>   2 * 4 + 1  =>  9        *********
-    }
-}
-create_pyramid_n_lines(10)
-// Mobile Legends  =>  Moba   =>  5x5
-// Very good strategy game    =>  enhance your brain and crytical thinking
-// Interesting  and  fun      =>  you will never get bored
-// ----------------------------------------------------------------
-// ----------------------------------------------------------------
+// TASK 2
+// function return_data_type(arr) {
+//     return typeof(arr[0])
+// }
+// let arr_of_numbers = ["23", 'hello', 90]
+// result = return_data_type(arr_of_numbers)
+// console.log(result)
+
+
+
+// TASK 3
+// function greatest_number(numbers) {
+//     let num = numbers[0]
+//     for (let item of numbers) {
+//         if (item > num) {
+//             num = item
+//         }
+//     }
+//     return num
+// }
+// let arr_of_numbers = [23, 6, 1, 15, 66, 123, 12, 55, 32]
+// let result = greatest_number(arr_of_numbers)
+// console.log(result)
+
+
+
+// TASK 4
+// function reversed_words(word) {
+//     let split_words = word.split('')
+//     let reverce_words = split_words.reverse()
+//     let join_words = reverce_words.join('')
+//     console.log(join_words)
+
+//     return join_words
+// }
+// let word = "Madina"
+// let result = reversed_words(word)
+// console.log(result)
+
+
+
+
+// function reverse_number(num) {
+//     let split_words = String(num).split('')
+//     let reverce_words = split_words.reverse()
+//     let join_words = reverce_words.join('')
+//     return parseInt(join_words)
+// }
+// let number = 12345
+// let result = reverse_number(number)
+// console.log(result)
+// ======================================================================
+// function swap_first_last(word) {
+//     // find first letter
+//     let first_letter = word[0]
+
+//     // find last letter
+//     let last_letter = word[word.length -1]
+
+//     // find middle letters
+//     let middle_letters = word.slice(1, word.length-1)
+//     return last_letter + middle_letters + first_letter
+// }
+// let word = "Hello world"
+// let result = swap_first_last(word)
+// console.log(result)
+// ======================================================================
+// Напишите функцию, которая принимает целое число и:
+//   - если число кратно 3, возвращает "Fizz";
+//   - если число кратно 5, возвращает "Buzz";
+//   - если число кратно и 3 и 5, возвращает "FizzBuzz";
+//   - если число не кратно ни 3, ни 5, возвращает само число.
+
+// function fizzBuzz(n) {
+//     if (n % 3 == 0 && n % 5 == 0) {
+//         console.log("Fizzbuzz")
+//     } else if (n % 3 == 0) {
+//         console.log("Fizz")
+//     } else if (n % 5 == 0) {
+//         console.log("Buzz")
+//     } else {
+//         console.log(n)
+//     }
+// }
+// fizzBuzz(1)
+// fizzBuzz(10)
+// fizzBuzz(15)
+
+
+
