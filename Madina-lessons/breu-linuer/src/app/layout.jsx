7a096297d0392e142d83e4f4f506517f113e71b3
Alisher <alisherxujanov163@gmail.com>
2024-11-06 08:30:24 +0500

    update

--- Madina-lessons/breu-linuer/src/app/layout.jsx
+++ Madina-lessons/breu-linuer/src/app/layout.jsx
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className='container'>
+      <body>
         <header>
           <Nav />
         </header>
