04d60f2f238dd15601e02f46b5e89e1c53003a93
Alisher <alisherxujanov163@gmail.com>
2025-01-30 06:29:55 +0500

    update

--- Madina-lessons/Vue/Ratatouille/vite.config.js
+++ Madina-lessons/Vue/Ratatouille/vite.config.js
@@ -17,4 +17,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@use "@/assets/styles/settings" as *;`
+  //     }
+  //   }
+  // }
 })
