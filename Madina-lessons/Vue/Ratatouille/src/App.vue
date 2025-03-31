c76638ac497ba616ecb7a881d5941c82d8556342
Alisher <alisherxujanov163@gmail.com>
2025-03-31 09:23:35 +0500

    update

--- Madina-lessons/Vue/Ratatouille/src/App.vue
+++ Madina-lessons/Vue/Ratatouille/src/App.vue
@@ -1,21 +1,20 @@
 <script setup>
 import MainLayout from './components/layouts/MainLayout.vue';
 import { provide } from 'vue';
-import mainBgImage from '@/assets/images/main-bg.png';
 
 const links = [
   {
-    image:` mainBgImage`,
+    image: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=2070&auto=format&fit=crop",
     linkText: "MENU",
     link: "/menu",
   },
   {
-    image: mainBgImage,
+    image: "https://images.unsplash.com/photo-1552566626-52f8b828add9?q=80&w=2070&auto=format&fit=crop",
     linkText: "RESERVATION",
     link: "/book-a-table",
   },
   {
-    image: mainBgImage,
+    image: "https://images.unsplash.com/photo-1559339352-11d035aa65de?q=80&w=1974&auto=format&fit=crop",
     linkText: "OUR RESTAURANT",
     link: "/about",
   }
